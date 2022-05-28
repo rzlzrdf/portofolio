f80774b235faf52c4dcbe2d61e580ea7cfbfc6cd
rzlzrdf <rizallazu@student.ub.ac.id>
2022-05-29 01:36:56 +0700

    step halaman portofolio - hero section

---
+++ Components/HeaderNav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import { Card, Col, Container, Nav, Navbar, NavItem, Row } from 'react-bootstrap'
+
+import style from './HeaderNav.module.css'
+
+
+const HeaderNav = () => {
+  return (
+    <Navbar expand="lg" variant='light' color='primary' className={style.container_nav}>
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" color='dark'/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className={"mx-auto gap-5 " + style.nav_bar}>
+              <Link href='/'><Nav.Link href="/">Portofolio</Nav.Link></Link>
+              <Link href='/gallery'><Nav.Link href="/gallery">Gallery</Nav.Link></Link>
+              <Link href='/about'><Nav.Link href="/about">Contact</Nav.Link></Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>     
+  )
+}
+
+export default HeaderNav
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import HeaderNav from '../Components/HeaderNav'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeaderNav />
+      <Component {...pageProps} />  
+    </>
+  
+  )
 }
 
 export default MyApp
