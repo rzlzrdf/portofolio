e4aeaa25cf4e8f2f788d77ef2ba367419f07c015
rzlzrdf <rizallazu@student.ub.ac.id>
2022-06-05 23:00:13 +0700

    add type effect and button for cv download

--- Components/HeaderNav.js
+++ Components/HeaderNav.js
@@ -7,14 +7,14 @@ import style from './HeaderNav.module.css'
 
 const HeaderNav = () => {
   return (
-    <Navbar expand="lg" variant='light' color='primary' className={style.container_nav}>
+    <Navbar expand="lg" variant='light' color='primary' className={style.container_nav + ' '}>
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" color='dark'/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className={"mx-auto gap-5 " + style.nav_bar}>
-              <Link href='/'><Nav.Link href="/">Portofolio</Nav.Link></Link>
-              <Link href='/gallery'><Nav.Link href="/gallery">Gallery</Nav.Link></Link>
-              <Link href='/about'><Nav.Link href="/about">Contact</Nav.Link></Link>
+              <Link href='/'><Nav.Link href="/" className={style.nav_barItem}>Portofolio</Nav.Link></Link>
+              <Link href='/gallery'><Nav.Link href="/gallery" className={style.nav_barItem}>Gallery</Nav.Link></Link>
+              <Link href='/about'><Nav.Link href="/about" className={style.nav_barItem}>Contact</Nav.Link></Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,54 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Card, Col, Container, Nav, Navbar, NavItem, Row } from 'react-bootstrap'
-import { VscFoldDown } from 'react-icons/vsc';
+import { Card, Col, Container, Button, Row } from 'react-bootstrap'
+import TypeAnimation from 'react-type-animation';
 import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
   return (
-    <Container>
-      <Row>
-        <Col lg='6' md='12'>
-          <h1 className={styles.title + ' fw-bold text-center'}>Hello There!</h1>
-          <h6 className={styles.subtitle1 + ' text-center'}>Welcome to my Portofolio</h6>
-          <p className={'blockquote-footer mt-4 text-center '+styles.name}>Rizal Lazuardi Firdaus</p>
-          <VscFoldDown className={styles.down + ' text-center'} size='40'  />
-        </Col>
-        <Col>
-          <img src='./hero.png' className={styles.hero_image +' '}  />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <section className={styles.hero1_container}>
+        <Container>
+          <Row>
+            <Col lg='7' md='12' className=''>
+              <h1 className={styles.title + ' text-start fw-bold'}>Hi, Im 
+                <span>
+                  <TypeAnimation
+                    cursor={true}
+                    sequence={[
+                      ' Rixzk..',
+                      2000, ' Rizal', 2000
+                    ]}
+                    wrapper="a"
+                    repeat={1}
+                  />
+                </span>
+              </h1>
+              <h6 className={styles.subtitle1 + ' '}>Im a <span>Frontend Web</span> Developer</h6>
+              <div className={styles.btnCvWrapper}>
+                <Button className={'' + styles.btnCv} variant="" >Download Resume</Button>
+              </div>
+            </Col>
+            <Col lg='5' md='12'>
+              <div className={styles.imgWrapper}>                
+                <img src='./hero.png' className={styles.hero_image +' '}  />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className={styles.hero2_container + ' bg-dark'}>
+        <Container className={styles.hero2} id='hero2' >
+          <Row>
+            <Col lg='5'>
+              <h3 className='text-center'>About Me</h3>
+              <h1 className='text-center'>Rizal Lazuardi Firdaus</h1>
+              <p>My name is Rizal Lazuardi Firdaus. Im a <strong>Frontend Web Developer</strong> based in <strong>Indonesia</strong>. </p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
